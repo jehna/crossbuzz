594e6eae7b53b86c03476830a4dd38cb13bc43a9
Jesse Luoto <jesse.luoto@gmail.com>
2024-05-23 23:10:03 +0300

    Fix crashing on prod

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,17 +9,6 @@ mod word_place;
 
 #[wasm_bindgen]
 pub fn solve(input: String, wordlist: Vec<String>) -> String {
-    let builder = thread::Builder::new()
-        .name("reductor".into())
-        .stack_size(1024 * 1024 * 1024);
-
-    let handler = builder.spawn(|| do_solve(input, wordlist)).unwrap();
-
-    let result = handler.join().unwrap();
-    result
-}
-
-pub fn do_solve(input: String, wordlist: Vec<String>) -> String {
     let input_places = input_places_from_visual(trim_indent_and_whitespace(input.as_str()));
     pretty_print(solver::solve(wordlist, input_places))
 }
